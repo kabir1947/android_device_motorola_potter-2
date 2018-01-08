514bb7583708472bd810a7b327df48f3edab6ef6
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-08 16:24:58 -0200

    potter: sepolicy: Remove leftovers from few services

--- device.mk
+++ device.mk
@@ -307,9 +307,6 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.mmi.touch.sh \
-    init.qcom.ril.sh \
-    init.qcom.bt.sh \
     init.qcom.fm.sh \
     wlan_carrier_bin.sh
 
@@ -319,7 +316,6 @@ PRODUCT_PACKAGES += \
     init.mmi.laser.sh \
     init.mmi.rc \
     init.mmi.usb.rc \
-    init.oem.hw.sh \
     init.qcom.rc \
     init.safailnet.rc \
     ueventd.qcom.rc
